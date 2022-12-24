ab3c85b2611485157dfb00c0749582aa86bdc27b
Fabiano Muffo <fabiano.tmuffo@sp.senac.br>
2022-12-24 14:35:28 -0300

    Aside bar items

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import * as S from './styles';
+import LogoImg from  '../../assets/logo.svg';
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp
+} from 'react-icons/md';
 
 function Aside() {
   return (
    <S.Wrapper>
-      <h1>Aside</h1>
+      <S.Header>
+        <S.Logo src={LogoImg} alt="My wallet logo"/>
+        <S.Title>My Wallet</S.Title>
+      </S.Header>
+      <S.MenuContainer>
+        <S.MenuItemLink>
+         <MdDashboard/> Dashboard
+        </S.MenuItemLink>
+        <S.MenuItemLink>
+          <MdArrowUpward/> Incomes
+        </S.MenuItemLink>
+        <S.MenuItemLink>
+        <MdArrowDownward/> Outcomes
+        </S.MenuItemLink>
+        <S.MenuItemLink>
+          <MdExitToApp/> Exit
+        </S.MenuItemLink>
+      </S.MenuContainer>
     </S.Wrapper>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,5 +3,45 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   grid-area: AS;
   background: ${props => props.theme.colors.secondary};
-  color:${props => props.theme.colors.white};
+  padding-left: 2em;
+  border-right:1px solid ${props => props.theme.colors.gray};
 `;
+
+export const Header = styled.header`
+  display:flex;
+  align-items: center;
+  height: 70px;
+`;
+export const Title = styled.h3`
+ color: ${props => props.theme.colors.white};
+ margin-left: 1em;
+`
+export const Logo = styled.img`
+  height:2em;
+  width:2em;
+`
+
+export const MenuContainer = styled.nav`
+  margin-top:50px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const MenuItemLink = styled.a`
+  color: ${props => props.theme.colors.info};
+  cursor: pointer;
+  margin:0.7em 0;
+  transition: opacity .3s;
+  font-size: 1.2em;
+  display: flex;
+  align-items: center;
+  &:hover {
+    opacity: .7;
+  }
+
+  > svg {
+    font-size: 1.5em;
+    margin-right: 0.3em;
+  }
+`
+
